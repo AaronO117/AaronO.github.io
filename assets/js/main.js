ce477b11e95e87a36b0f8e2b6d348b132153aa67
aaronoropeza <aoropeza@uwm.edu>
2024-05-27 18:40:49 -0500

    complete set up

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Example: Update location dynamically
+    const locationElement = document.getElementById("location");
+    if (locationElement) {
+        locationElement.textContent = "cloudy";
+    }
+});
